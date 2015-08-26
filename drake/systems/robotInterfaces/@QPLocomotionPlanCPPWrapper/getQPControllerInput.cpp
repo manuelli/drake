90be5461c9f5f96e23e180aa7ab3c0554628ba17
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2015-08-26 12:10:01 -0400

    rever more files back to master

--- drake/systems/robotInterfaces/@QPLocomotionPlanCPPWrapper/getQPControllerInput.cpp
+++ drake/systems/robotInterfaces/@QPLocomotionPlanCPPWrapper/getQPControllerInput.cpp
@@ -6,8 +6,8 @@ using namespace Eigen;
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-  if (nrhs != 5 || nlhs != 1) {
-    mexErrMsgTxt("usage: lcm_msg_data = getQPControllerInput(obj, t_global, x, contact_force_detected, byte_array);");
+  if (nrhs != 4 || nlhs != 1) {
+    mexErrMsgTxt("usage: lcm_msg_data = getQPControllerInput(obj, t_global, x, contact_force_detected);");
   }
 
   QPLocomotionPlan* plan = (QPLocomotionPlan*) getDrakeMexPointer(mxGetPropertySafe(prhs[0], "qp_locomotion_plan_ptr"));
@@ -17,17 +17,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   auto q = Map<const VectorXd>(mxGetPrSafe(prhs[2]), nq);
   auto v = Map<const VectorXd>(mxGetPrSafe(prhs[2]) + nq, nv);
   auto contact_force_detected = matlabToStdVector<bool>(prhs[3]);
-
-
-  shared_ptr<drc::robot_state_t> robot_state(new drc::robot_state_t());
-  const mxArray* lcm_message_mex = prhs[4];
-  if (!mxIsInt8(lcm_message_mex))
-    mexErrMsgTxt("Expected an int8 array as the qp_input argument");
-  robot_state->decode(mxGetData(lcm_message_mex), 0, mxGetNumberOfElements(prhs[narg]));
-
-
-
-  drake::lcmt_qp_controller_input qp_controller_input = plan->createQPControllerInput(t_global, q, v, contact_force_detected, robot_state);
+  drake::lcmt_qp_controller_input qp_controller_input = plan->createQPControllerInput(t_global, q, v, contact_force_detected);
   const size_t size = qp_controller_input.getEncodedSize();
   plhs[0] = mxCreateNumericMatrix(size, 1, mxUINT8_CLASS, mxREAL);
   qp_controller_input.encode(mxGetData(plhs[0]), 0, static_cast<int>(size));
