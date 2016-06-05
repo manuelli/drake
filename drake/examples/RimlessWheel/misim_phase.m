16f374194a7a972bf05784ecc009c16f9bacfb13
Russ Tedrake <russt@mit.edu>
2016-06-04 22:50:06 -0400

    linearized constraints works well.  comparable phase plots... but initial conditions cause the linearized model to jump into the air (plot just the z coordinate to see).

--- drake/examples/RimlessWheel/misim_phase.m
+++ drake/examples/RimlessWheel/misim_phase.m
@@ -8,12 +8,18 @@ p = TimeSteppingRigidBodyManipulator('RimlessWheel.urdf',.01,options);
 warning(w);
 
 figure(1); clf;  hold on;
-for thetadot0 = 0:0.1:1
-  x0 = [0;1;0;thetadot0;0;thetadot0];
-  xtraj = misim(getManipulator(p),x0,.02,140);
-  fnplt(xtraj,[3,6]);
+for thetadot0 = 0:0.075:1
+  theta0 = 0.2;
+  x0 = [sin(theta0);cos(theta0);theta0;thetadot0;0;thetadot0];
+  xtraj = misim(getManipulator(p),x0,.02,300);
+  h=fnplt(xtraj,[3,6]);  set(h,'LineStyle','-','MarkerSize',10)
+  xtrajlin = milinsim(getManipulator(p),[sin(pi/8);cos(pi/8);pi/8],x0,.02,300);
+  h=fnplt(xtrajlin,[3,6]); set(h,'LineStyle','-','MarkerSize',10,'Color','r');
+  drawnow; 
 end
-  
+
+axis([0 pi/4 -1 1.5])
+
 %v = p.constructVisualizer();
 %v.axis = [-2.5 2.5 -.1 3];
 %v.playback(xtraj);
---
+++ drake/systems/plants/@RigidBodyManipulator/milinsim.m
@@ -0,0 +1,153 @@
+function traj = milinsim(obj,qstar,x0,h,N,mu)
+% quick demo of mixed-integer-based simulation through contact
+% todo: include control inputs (current sets u=0)
+% todo: work this into a proper time-stepping system
+% todo: add friction cone (currently assumes infinite friction)
+
+nq = getNumPositions(obj);
+nv = getNumVelocities(obj);
+assert(numel(qstar)==nq);
+q = x0(1:nq);
+v = x0(nq+1:end);
+assert(numel(v)==nv);
+
+% compute linearization of constraints
+[H,C,~,~,dC] = manipulatorDynamics(obj,qstar,zeros(nv,1));
+[phi,~,~,~,~,~,~,~,n,D,dn,dD] = obj.contactConstraints(qstar,false);
+nc = numel(phi);
+nd = numel(D)/2;
+d = cell2mat(D(1:nd));
+
+% compute zn0 (assume zf0 = 0 for now) with C = n'*zn0.
+zn0 = zeros(nc,1);  bzn0 = phi<1e-5;
+zn0(bzn0) = n(bzn0,:)'\C;
+
+Aq = dC(:,1:nq);
+for ci=find(bzn0)'
+  Aq = Aq - dn(nq*(ci-1)+(1:nq),:)*zn0(ci);
+end
+Av = -H/h + dC(:,nq+(1:nv));
+Avn = H/h;
+
+if (nargin<6), mu = 1; end
+
+% variable indices
+vn_inds = 1:nv;
+normal_force_inds = vn_inds(end)+(1:nc);
+friction_force_inds = normal_force_inds(end)+(1:nc*nd);
+binary_normal_inds = friction_force_inds(end)+(1:nc);
+binary_pos_slide_inds = binary_normal_inds(end)+(1:nc*nd);
+binary_neg_slide_inds = binary_pos_slide_inds(end)+(1:nc*nd);
+num_vars = binary_neg_slide_inds(end);
+
+% constraint indices
+dynamic_inds = 1:nv;
+no_normal_force_ub_inds = dynamic_inds(end)+(1:nc);
+nonpen_lb_inds = no_normal_force_ub_inds(end)+(1:nc);
+nonpen_ub_inds = nonpen_lb_inds(end)+(1:nc);
+nonslide_lb_inds = nonpen_ub_inds(end)+(1:nc*nd);
+nonslide_ub_inds = nonslide_lb_inds(end)+(1:nc*nd);
+pos_slide_force_lb_inds = nonslide_ub_inds(end)+(1:nc*nd);
+pos_slide_force_ub_inds = pos_slide_force_lb_inds(end)+(1:nc*nd);
+neg_slide_force_lb_inds = pos_slide_force_ub_inds(end)+(1:nc*nd);
+neg_slide_force_ub_inds = neg_slide_force_lb_inds(end)+(1:nc*nd);
+num_constraints = neg_slide_force_ub_inds(end);
+
+bigM = 5e2;
+
+% setup model
+model.vtype = repmat('C',num_vars,1);
+model.vtype([binary_normal_inds,binary_pos_slide_inds,binary_neg_slide_inds]) = 'B';
+model.obj = zeros(num_vars,1);
+model.A = sparse(num_constraints,num_vars);
+model.rhs = zeros(num_constraints,1);
+model.sense(dynamic_inds)='=';
+model.sense([nonpen_lb_inds,nonslide_lb_inds,pos_slide_force_lb_inds,neg_slide_force_lb_inds])='>';
+model.sense([no_normal_force_ub_inds,nonpen_ub_inds,nonslide_ub_inds,pos_slide_force_ub_inds,neg_slide_force_ub_inds])='<';
+
+model.lb = -inf(num_vars,1);  model.lb(normal_force_inds)=0;
+model.ub = inf(num_vars,1);
+
+%% normal force <= binary_normal*bigM
+model.A(no_normal_force_ub_inds,normal_force_inds) = eye(nc);
+model.A(no_normal_force_ub_inds,binary_normal_inds) = -eye(nc)*bigM;
+% rhs = 0 already
+
+%% 0 <= phi + h*n*vn <= binary_normal*bigM
+%% linearized to phi0 + n*(q-qstar) + h*n*vn 
+model.A(nonpen_ub_inds,binary_normal_inds) = eye(nc)*bigM;
+
+%% -binary_neg_slide*bigM <= dk*vn <= binary_pos_slide*bigM 
+model.A(nonslide_lb_inds,binary_neg_slide_inds) = eye(nc*nd)*bigM;  
+% rhs = 0 already
+model.A(nonslide_ub_inds,binary_pos_slide_inds) = -eye(nc*nd)*bigM;
+% rhs = 0 already
+
+%% -mu*zn <= zf <= -mu*zn + (1-binary_pos_slide)*M
+model.A(pos_slide_force_lb_inds,normal_force_inds) = mu*repmat(eye(nc),nd,1);
+model.A(pos_slide_force_lb_inds,friction_force_inds) = eye(nc*nd);
+% rhs = 0 already
+
+model.A(pos_slide_force_ub_inds,normal_force_inds) = mu*repmat(eye(nc),nd,1);
+model.A(pos_slide_force_ub_inds,friction_force_inds) = eye(nc*nd);
+model.A(pos_slide_force_ub_inds,binary_pos_slide_inds) = eye(nc*nd)*bigM;
+model.rhs(pos_slide_force_ub_inds) = bigM;
+
+%% mu*zn - (1-binary_neg_slide)*bigM <= zf <= mu*zn
+model.A(neg_slide_force_lb_inds,normal_force_inds) = -mu*repmat(eye(nc),nd,1);
+model.A(neg_slide_force_lb_inds,friction_force_inds) = eye(nc*nd);
+model.A(neg_slide_force_lb_inds,binary_neg_slide_inds) = -eye(nc*nd)*bigM;
+model.rhs(neg_slide_force_lb_inds) = -bigM;
+
+model.A(neg_slide_force_ub_inds,normal_force_inds) = -mu*repmat(eye(nc),nd,1);
+model.A(neg_slide_force_ub_inds,friction_force_inds) = eye(nc*nd);
+% rhs = 0 already
+ 
+params.outputflag = 0;  % silent output
+
+xx = repmat(double(x0),1,N+1);
+
+for i=1:N
+  model.A(dynamic_inds,vn_inds) = H/h;
+  model.A(dynamic_inds,normal_force_inds) = -n';
+  model.A(dynamic_inds,friction_force_inds) = -d';
+  model.rhs(dynamic_inds) = -C - Aq*(q-qstar) - Av*v ; 
+
+  model.A(nonpen_lb_inds,vn_inds) = h*n;
+  model.rhs(nonpen_lb_inds) = -phi - n*(q-qstar);
+  model.A(nonpen_ub_inds,vn_inds) = h*n;
+  model.rhs(nonpen_ub_inds) = bigM - phi - n*(q-qstar);
+  
+  model.A(nonslide_lb_inds,vn_inds) = d;
+  model.A(nonslide_ub_inds,vn_inds) = d;
+  
+%   % for debugging
+%   bnorm = (phi<.1);
+%   bpos = zeros(nc*nd,1);
+%   bneg = zeros(nc*nd,1);
+%   model2 = model;
+%   model2.rhs = -model.A(:,[binary_normal_inds,binary_pos_slide_inds,binary_neg_slide_inds])*[bnorm;bpos;bneg];
+%   model2.A(:,[binary_normal_inds,binary_pos_slide_inds,binary_neg_slide_inds])=[];
+%   model2.vtype([binary_normal_inds,binary_pos_slide_inds,binary_neg_slide_inds])=[];
+%   model2.obj([binary_normal_inds,binary_pos_slide_inds,binary_neg_slide_inds])=[];
+%   model2.lb([binary_normal_inds,binary_pos_slide_inds,binary_neg_slide_inds])=[];
+%   model2.ub([binary_normal_inds,binary_pos_slide_inds,binary_neg_slide_inds])=[];
+%   % end debugging
+  
+  
+  result = gurobi(model,params);
+%  [q',v']
+%   contact = [result.x(binary_normal_inds)';
+%     result.x(binary_pos_slide_inds)';
+%     result.x(binary_neg_slide_inds)']
+%  zn = result.x(zn_inds)'
+%  zf = result.x(zf_inds)'
+  v = result.x(vn_inds);
+  q = q+h*v;
+  
+  xx(:,i+1)=[q;v];
+%  keyboard
+end
+
+traj = DTTrajectory(h*(0:N),xx);
+traj = setOutputFrame(traj,getStateFrame(obj));
