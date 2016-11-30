740bacc1ca5886d6eccb035c7a3f163b41d226c0
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2016-11-30 18:45:14 -0500

    lots of updates for timestepping simulation with uncertainty

--- drake/examples/CompassGait/TimeSteppingCompassGaitPlant.m
+++ drake/examples/CompassGait/TimeSteppingCompassGaitPlant.m
@@ -4,6 +4,8 @@ classdef TimeSteppingCompassGaitPlant < CompassGaitPlant
     odefun;
     eventFun;
     odeOptions;
+    cgUtils_
+    stepSize_ = 0.002;
   end
   methods
     function obj = TimeSteppingCompassGaitPlant(gammaIn)
@@ -11,6 +13,7 @@ classdef TimeSteppingCompassGaitPlant < CompassGaitPlant
       obj.dataHandle = SharedDataHandle(struct());
       obj.dataHandle.data.u = 0;
       obj = obj.initializeDynamicsAndEventFunctions();
+      obj.cgUtils_ = CompassGaitUtils();
     end
 
     function obj = initializeDynamicsAndEventFunctions(obj)
@@ -31,14 +34,44 @@ classdef TimeSteppingCompassGaitPlant < CompassGaitPlant
       % x_initial should be of size 4;
       
       obj.dataHandle.data.u = u;      
-      [t,y, te, ye, ie] = ode45(obj.odefun, tspan, x_initial, obj.odeOptions);
-            
+      [t,y, te, ye, ie] = ode45(obj.odefun, tspan, x_initial, obj.odeOptions);            
     end
 
+    % do some extra checking to see if we have changed modes, if so then run ode45
+    function [t, y, hybridEvent] = simulateWithConstantControlInputODE4(obj, x_initial, tspan, u, processNoiseLocal, dt)
 
-    function simulateODE(obj, x_initial_full, tspan)
+      if nargin < 6
+        dt = obj.stepSize_;
+      else
+        dt = options.dt;
+      end
 
+
+      odeFunStochastic = @(t,x) obj.dynamics(t,[1;x],0) + processNoiseLocal;
+
+      hybridEvent = false;
+
+      % x_initial should be of size 4;
+      obj.dataHandle.data.u = u;
+      numSteps = max(ceil((tspan(2) - tspan(1))/dt),2);
+      tspanFull = linspace(tspan(1),tspan(2),numSteps);
+      
+      obj.dataHandle.data.u = u;      
+      [t,y] = ode4(odeFunStochastic, tspanFull, x_initial);
+
+      % check if we had a zero crossing and do some extra work.
+      yPrime = y';
+      x_final = yPrime(:,end);
+      guardVal = obj.eventFunction(0,x_final,0);
+
+      % this means we had a zero crossing, so redo the work using ode45
+      if (guardVal < 0)
+        [t,y, te, ye, ie] = ode45(odeFunStochastic, tspan, x_initial, obj.odeOptions);
+        hybridEvent = true;
+        disp('falling back to ode45')
+      end
     end
+
 
     function [value, isTerminal, direction] = eventFunction(obj, t, x, u)
       g1 = obj.footCollisionGuard1(t,x,u);
@@ -53,5 +86,63 @@ classdef TimeSteppingCompassGaitPlant < CompassGaitPlant
       uController = obj.dataHandle.data.u; % the stored control input for this short simulation
       xcdot = dynamics@CompassGaitPlant(obj, t, x, uController);
     end
+
+    % processNoise should be of the same dimension as xcdot
+    % should be specified once at beginning of the integration
+    % the processNoise should be in local coords
+    function [t,y, te, ye, ie] = simulateWithConstantControlInputODEStochastic(obj, x_initial, tspan, u, processNoiseLocal)
+
+      obj.dataHandle.data.u = u;
+      odeFunStochastic = @(t,x) obj.dynamics(t,[1;x],0) + processNoiseLocal;
+      [t,y, te, ye, ie] = ode45(odeFunStochastic, tspan, x_initial, obj.odeOptions);
+    end
+
+
+    % basically the same as simulateWithConstantControlInputODE but it doesn't just 
+    % return when it hits the hybrid guard, it simulates through it.
+    % Everything is in LOCAL coordinates, (theta_swing, theta_stance)
+    % x_initial is in local coordinates already
+    % only need the hybrid mode for mapping the control input from global --> local
+    function [x_final, hybridSwitch, finalHybridMode] = simulateThroughHybridEvent(obj,hybridMode, x_initial, tspan, uGlobal, processNoiseGlobal)
+
+      if (nargin < 6)
+        processNoiseGlobal = zeros(4,1);
+      end
+
+      uLocal = obj.cgUtils_.transformGlobalControlToLocalControl(hybridMode, uGlobal);
+
+      % probably don't need to do this, variance for left and right will be the same
+      % processNoiseLocal = obj.cgUtils.transformVectorGlobalToLocal(hyrbidMode, processNoiseGlobal);
+
+
+      % [t,y, te, ye, ie] = obj.simulateWithConstantControlInputODEStochastic(x_initial,tspan,uLocal, processNoiseGlobal); 
+
+      [t,y,hybridSwitch] = obj.simulateWithConstantControlInputODE4(x_initial,tspan,uLocal, processNoiseGlobal);
+
+
+      yPrime = y';
+      x_final = yPrime(:,end);
+      finalHybridMode = hybridMode;
+
+      % figure out if we need to simulate again because of hitting the hybrid guard
+      if( hybridSwitch > 0)
+        hybridSwitch = true;
+        finalHybridMode = obj.cgUtils_.getOtherHybridMode(hybridMode);
+        uLocal = obj.cgUtils_.transformGlobalControlToLocalControl(finalHybridMode, uGlobal);
+        [xp, ~] = obj.collisionDynamics(1, 0, x_final);
+        t_hybrid_guard = t(end);
+        tspan_post_impact = [t_hybrid_guard, tspan(2)];
+
+
+        % [t,y,te,ye,ie] = obj.simulateWithConstantControlInputODEStochastic(xp, tspan_post_impact, uLocal, processNoiseGlobal);
+
+        [t,y,~] = obj.simulateWithConstantControlInputODE4(xp, tspan_post_impact, uLocal, processNoiseGlobal);
+
+        yPrime = y';
+        x_final = yPrime(:,end);
+      end
+    end
+
+
   end
 end
