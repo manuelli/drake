92afc2c91460d3aa37c2a3e30d77b7ed5d18a846
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2017-01-20 19:48:01 -0500

    write torques into EST_ROBOT_STATE msg

--- drake/examples/kuka_iiwa_arm/kuka_iiwa_state_translator.py
+++ drake/examples/kuka_iiwa_arm/kuka_iiwa_state_translator.py
@@ -8,6 +8,12 @@ import drake as lcmdrake
 def onIiwaStatus(msg):
     q = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0] + list(msg.joint_position_measured)
     stateMsg = robotstate.drakePoseToRobotState(q)
+    numJoints = len(msg.joint_position_measured)
+
+    # doesn't publish velocity for some reason
+    for i in xrange(0,numJoints):
+    	stateMsg.joint_effort[i] = msg.joint_torque_measured[i]
+
     stateMsg.utime = msg.utime
     lcmUtils.publish('EST_ROBOT_STATE', stateMsg)
 
