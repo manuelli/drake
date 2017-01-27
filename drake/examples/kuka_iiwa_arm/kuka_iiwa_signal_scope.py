d4c453685f34a7c054cc39e84bec063b94db3cef
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2017-01-27 14:43:07 -0500

    add some plotting utilities

--- drake/examples/kuka_iiwa_arm/kuka_iiwa_signal_scope.py
+++ drake/examples/kuka_iiwa_arm/kuka_iiwa_signal_scope.py
@@ -5,7 +5,7 @@ import colorsys
 
 addPlot(timeWindow=15, yLimits=[-3.14, 3.14])
 addSignals('IIWA_STATUS', msg.utime, msg.joint_position_measured, range(7))
-addSignals('IIWA_STATUS', msg.utime, msg.joint_position_commanded, range(7))
+addSignals('IIWA_STATUS', msg.utime, msg.joint_effort, range(7))
 
 
 addPlot(timeWindow=15, yLimits=[-3.14, 3.14])
