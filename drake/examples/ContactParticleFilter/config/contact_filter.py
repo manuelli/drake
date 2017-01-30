5cecb63737f005b70b0c63d9b1df64724a76799e
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2017-01-30 16:11:43 -0500

    update plotting and config files

--- drake/examples/ContactParticleFilter/config/contact_filter.py
+++
@@ -1,7 +0,0 @@
-
-jointIdx = range(7)
-addPlot(timeWindow=10, yLimits=[-50,50])
-# addSignals('RESIDUAL_OBSERVER_STATE', msg.utime, msg.residual, joints, keyLookup=names)
-# addSignals('RESIDUAL_OBSERVER_STATE_W_FOOT_FORCE', msg.utime, msg.residual, joints, keyLookup=names)
-addSignals('RESIDUAL_OBSERVER_STATE', msg.utime, msg.residual, joints, keyLookup=names)
-addSignals('RESIDUAL_ACTUAL', msg.utime, msg.residual, joints, keyLookup=names)
