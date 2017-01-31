a43b481a9fea7eb68dc282865a431267703f5f9c
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2017-01-31 14:19:23 -0500

    small updates and bug fixes

--- drake/examples/ContactParticleFilter/python/director_cpf_launch.py
+++ drake/examples/ContactParticleFilter/python/director_cpf_launch.py
@@ -1,6 +1,11 @@
 __author__ = 'manuelli'
-import os
 
+"""
+Launches a director instance with additional classes relevant to Contact Particle Filter.
+Calls director with --startup option and file runContactFilterStartup.py
+"""
+
+import os
 def main():
 
     drake_source_dir = os.getenv('DRAKE_SOURCE_DIR')
@@ -12,7 +17,7 @@ def main():
     # director_config_filename = 'drake/examples/kuka_iiwa_arm/director_config.json'
     director_kuka_command = "directorPython $DRAKE_SOURCE_DIR/drake/examples/kuka_iiwa_arm/director_ik_app.py" \
                             " --director_config $DRAKE_SOURCE_DIR/drake/examples/kuka_iiwa_arm/director_config.json"
-    startup_command = " --startup $DRAKE_SOURCE_DIR/drake/examples/ContactParticleFilter/src/runContactParticleFilterStartup.py"
+    startup_command = " --startup $DRAKE_SOURCE_DIR/drake/examples/ContactParticleFilter/python/runContactParticleFilterStartup.py"
 
 
     shell_command = director_kuka_command + startup_command
--- drake/examples/ContactParticleFilter/python/runContactParticleFilterStartup.py
+++ drake/examples/ContactParticleFilter/python/runContactParticleFilterStartup.py
@@ -1,9 +1,21 @@
 __author__ = 'manuelli'
 
+# this is startup script that gets executed on top of director_ik_app.py using --startup flag
+"""
+Script passed in with --startup option to directorPython that creates classes
+relevant for the Contact Particle Filter
+
+Usage
+-------
+used together with director_cpf_launch.py
+"""
+
 import os
 import sys
+
+# add relevant directories to path so Python imports can find them
 DRAKE_SOURCE_DIR = os.getenv('DRAKE_SOURCE_DIR')
-sys.path.append(os.path.join(DRAKE_SOURCE_DIR, 'drake/examples/ContactParticleFilter/src'))
+sys.path.append(os.path.join(DRAKE_SOURCE_DIR, 'drake/examples/ContactParticleFilter/python'))
 
 import contactparticlefilterstartup
 contactparticlefilterstartup.startup(robotSystem, globals())
