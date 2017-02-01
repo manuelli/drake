36af2c5706b4e50eb44be526cf7878c175ec4f89
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2017-02-01 11:31:23 -0500

    pass in globals dict

--- drake/examples/kuka_iiwa_arm/director_ik_app.py
+++ drake/examples/kuka_iiwa_arm/director_ik_app.py
@@ -3,8 +3,6 @@ Usage: This program should be launched using the command line specified in the
        kuka_sim.pmd file.
 '''
 
-import time
-
 from director import mainwindowapp
 from director import robotsystem
 from director import applogic
@@ -23,48 +21,33 @@ class KukaSimInfoLabel(object):
         self.label = QtGui.QLabel('')
         statusBar.addPermanentWidget(self.label)
 
-        self.sub = lcmUtils.addSubscriber(
-            'IIWA_STATUS', lcmdrake.lcmt_iiwa_status, self.onIiwaStatus)
+        self.sub = lcmUtils.addSubscriber('IIWA_STATUS',
+                                  lcmdrake.lcmt_iiwa_status, self.onIiwaStatus)
         self.sub.setSpeedLimit(30)
 
         self.label.text = '[waiting for sim status]'
 
     def onIiwaStatus(self, msg):
-        simTime = msg.utime * 1e-6
+        simTime = msg.utime*1e-6
         simFreq = self.sub.getMessageRate()
         self.label.text = 'Sim freq: %d hz  |  Sim time: %.2f' % (simFreq,
                                                                   simTime)
 
 
-def sendGripperCommand(targetPositionMM, force):
-    msg = lcmdrake.lcmt_schunk_wsg_command()
-    msg.utime = int(time.time() * 1e6)
-    msg.force = force
-    msg.target_position_mm = targetPositionMM
-    lcmUtils.publish('SCHUNK_WSG_COMMAND', msg)
-
-
-def gripperOpen():
-    sendGripperCommand(100, 40)
-
-
-def gripperClose():
-    sendGripperCommand(15, 40)
-
-
-def setupToolbar():
-    toolBar = applogic.findToolBar('Main Toolbar')
-    app.app.addToolBarAction(
-        toolBar, 'Gripper Open', icon='', callback=gripperOpen)
-    app.app.addToolBarAction(
-        toolBar, 'Gripper Close', icon='', callback=gripperClose)
+def makeRobotSystem(view):
+    factory = robotsystem.RobotSystemFactory()
+    options = factory.getDisabledOptions()
+    factory.setDependentOptions(options, usePlannerPublisher=True,
+                                         useTeleop=True)
+    return factory.construct(view=view, options=options)
 
 
 # create a default mainwindow app
-app = mainwindowapp.construct()
+app = mainwindowapp.MainWindowAppFactory().construct(globalsDict=globals())
+mainwindowapp.MainWindowPanelFactory().construct(app=app.app, view=app.view)
 
 # load a minimal robot system with ik planning
-robotSystem = robotsystem.create(app.view, planningOnly=True)
+robotSystem = makeRobotSystem(app.view)
 
 # add the teleop and playback panels to the mainwindow
 app.app.addWidgetToDock(robotSystem.teleopPanel.widget,
@@ -72,28 +55,22 @@ app.app.addWidgetToDock(robotSystem.teleopPanel.widget,
 app.app.addWidgetToDock(robotSystem.playbackPanel.widget,
                         QtCore.Qt.BottomDockWidgetArea)
 
-setupToolbar()
-
 # show sim time in the status bar
 infoLabel = KukaSimInfoLabel(app.mainWindow.statusBar())
 
 # use pydrake ik backend
 ikPlanner = robotSystem.ikPlanner
-if ikPlanner.planningMode == 'pydrake':
-    ikPlanner.plannerPub._setupLocalServer()
+ikPlanner.planningMode = 'pydrake'
+ikPlanner.plannerPub._setupLocalServer()
 
 # change the default animation mode of the playback panel
 robotSystem.playbackPanel.animateOnExecute = True
 
 # disable pointwise ik by default
-ikPlanner.getIkOptions().setProperty('Use pointwise', False)
-ikPlanner.getIkOptions().setProperty('Max joint degrees/s', 60)
-
-# initialize the listener for the pose gui
-ikPlanner.addPostureGoalListener(robotSystem.robotStateJointController)
+robotSystem.ikPlanner.getIkOptions().setProperty('Use pointwise', False)
 
 # set the default camera view
-applogic.resetCamera(viewDirection=[-1, 0, 0], view=app.view)
+applogic.resetCamera(viewDirection=[-1,0,0], view=app.view)
 
 # start!
 app.app.start()
